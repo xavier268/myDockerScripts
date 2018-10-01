4639ef99fe240edf62cbfd94922e21112276cd49
Xavier <xavier@fed28XPS>
2018-10-01 21:44:48 +0200

    docker-machine

--- dockerMachine/create.sh
+++ dockerMachine/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Constructing an aws docker machine"
+echo "Constructing an aws docker machine named : $1"
 docker-machine create \
         --driver amazonec2 \
         --amazonec2-instance-type 	"t2.micro" \
@@ -8,6 +8,9 @@ docker-machine create \
 	--amazonec2-open-port 		4444 \
 	--amazonec2-zone		a\
 	--amazonec2-vpc-id		vpc-41eab224 \
-					aws-test
+					$1
+docker-machine ls
+docker-machine env $1 
+echo "type 'docker-machine env -u' to unset the docker client"
 
 
